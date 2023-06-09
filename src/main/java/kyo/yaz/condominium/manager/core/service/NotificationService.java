76dc5199ef47baf7ba7d73765cb23810986bc455
Yazal Ulloa <yazalulloa@gmail.com>
2023-06-09 01:00:05 -0400

    fix notification service

--- src/main/java/kyo/yaz/condominium/manager/core/service/NotificationService.java
+++ src/main/java/kyo/yaz/condominium/manager/core/service/NotificationService.java
@@ -2,18 +2,23 @@ package kyo.yaz.condominium.manager.core.service;
 
 import io.reactivex.rxjava3.core.Completable;
 import java.util.concurrent.TimeUnit;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class NotificationService {
 
-  @Value("${app.notification_chat_id}")
+
   private final long chatId;
 
   private final TelegramRestApi restApi;
+
+  @Autowired
+  public NotificationService(@Value("${app.notification_chat_id}") long chatId, TelegramRestApi restApi) {
+    this.chatId = chatId;
+    this.restApi = restApi;
+  }
 
   public boolean sendBlocking(String msg) {
     return send(msg)
