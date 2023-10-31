f00f71fd515175bac9c70fc19cb5234047bb6857
Yazal Ulloa <yazalulloa@gmail.com>
2023-10-31 10:43:37 -0400

    NetworkUtil change

--- src/main/java/kyo/yaz/condominium/manager/core/util/NetworkUtil.java
+++ src/main/java/kyo/yaz/condominium/manager/core/util/NetworkUtil.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+import kyo.yaz.condominium.manager.core.util.rx.RetryWithDelay;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -26,6 +28,8 @@ public class NetworkUtil {
     log.info(timeZone.toString());
     TimeZone.setDefault(timeZone);
     Single.fromCallable(NetworkUtil::getPublicIp)
+        .doOnError(throwable -> log.error("FAILED_TO_GET_PUBLIC_IP", throwable))
+        .retryWhen(RetryWithDelay.retry(500, TimeUnit.MILLISECONDS))
         .subscribeOn(Schedulers.io())
         .doOnSuccess(EnvUtil::saveCurrentIp)
         .subscribe(ip -> log.info("PUBLIC_IP {}", ip), throwable -> log.error("FAILED_TO_GET_PUBLIC_IP", throwable));
